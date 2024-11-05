186e4423287a4a9bd54af6e04d068eaf39077f79
Muhammedjashir <jashirmhd71@gmail.com>
2024-11-05 11:55:24 +0530

    manage orders

--- E-Commerce-main/src/AdminPagesComponent/ManageOrders.jsx
+++ E-Commerce-main/src/AdminPagesComponent/ManageOrders.jsx
@@ -16,7 +16,7 @@ import {
 function ManageOrders() {
   const [order, setOrder] = useState([]);
 
-  const OrderDatas = async () => {
+  const OrderDatas = async ()=> {
     try {
       const response = await axios.get("http://localhost:4000/users");
       const orders = response.data;
